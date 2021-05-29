2918f676329b716f0c852fb9af00e774e7d3d274
Mr Strange <77616081+Strangx@users.noreply.github.com>
2021-05-29 17:43:56 +0630

    Update Httpbackend.js

--- Httpbackend.js
+++ Httpbackend.js
@@ -1,4 +1,1 @@
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+document.write("Hello World!");
